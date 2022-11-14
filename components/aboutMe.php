0add49e5e3996740d55139b30a86c8c3a7f83ce8
eazycchi <mochizza123@gmail.com>
2022-11-14 15:58:49 +0700

    absen sore porto

--- components/aboutMe.php
+++ components/aboutMe.php
@@ -6,11 +6,11 @@
                 Hai, Saya Mochi
             </div>
             <div
-                class="pl-5 text-2xl font-bold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">
-                Saya adalah junior web developer</div>
+                class="pl-5  text-2xl font-bold tracking-tight leading-relaxed text-gray-600 md:text-3xl lg:text-4xl dark:text-white">
+                Saya adalah junior web developer. Jangan samakan saya dengan para pemula itu. Saya adalah beta tester yang berhasil mencapai lantai paling atas.</div>
         </div>
         <div>
-            <img id="fadeGambar" src="assets/gambar.png" alt="Gambar Kirito">
+            <img id="fadeGambar" src="assets/Logo Mochi.png" alt="Gambar Kirito" class="rotate-90">
         </div>
     </div>
 </div>
