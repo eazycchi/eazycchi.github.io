6696efe24cf640a413ff2a532f4575b1909ccc19
eazycchi <mochizza123@gmail.com>
2022-11-18 16:18:02 +0700

    make background

--- components/aboutMe.php
+++ components/aboutMe.php
@@ -1,12 +1,15 @@
-<div class="sm:grid grid-cols-1 md:grid grid-cols-2">
-    <div>
-        <div class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 mb-2 m-20 text-4xl font-extrabold tracking-tight leading-none dark:text-white text-gray-900 md:text-5xl lg:text-6xl">
-            Hai, Saya Mochi
+<div class="grid grid-cols-2 sm:grid-cols-1">
+    <div class="">
+        <div class="mb-2 m-20 text-4xl font-extrabold tracking-tight leading-none dark:text-white text-gray-900 md:text-3xl lg:text-3xl">
+            <span class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"> Hai, Saya Mochi</span>
         </div>
-        <div id="fadeNama" class="dark:text-white mt-5 m-20 text-2xl font-bold tracking-tight leading-relaxed text-gray-600 md:text-3xl lg:text-4xl">
+        <div id="fadeNama" class="dark:text-white mt-5 m-20 text-2xl font-bold tracking-tight leading-relaxed text-gray-600 md:text-xl lg:text-2xl">
             Saya adalah junior web developer. Jangan samakan saya dengan para pemula itu. Karena saya masih kalah jauh sama mereka. *muka sedih*</div>
+        <hr id="fadeNama" class="mt-5 m-20">
+        <div id="fadeNama" class="dark:text-white mt-5 m-20 text-2xl font-bold tracking-tight leading-relaxed text-gray-600 md:text-xl lg:text-2xl">
+            Kalo web ini udah jadi, gw mau copas, gw mau taruh ke linkedin gw, gamau tau, atau pemilik akun ini harus bayar 500 juta. <br>-Gw hacker
+        </div>
     </div>
-
-    <div><img class="-z-50" id="fadeGambar" src="/assets/img/LogoMochi.png" alt=""></div>
-
 </div>
+<img class="-z-50" id="fadeAbout" src="/assets/img/LogoMochi.svg" alt="">
+<img class="-z-50" id="fadeAbout2" src="/assets/img/LogoMochi.svg" alt="">
