4dee51b2a5886887d255981e8cfd8ca481c97972
eazycchi <mochizza123@gmail.com>
2022-11-18 14:22:23 +0700

    add dark mode

--- components/aboutMe.php
+++ components/aboutMe.php
@@ -1,5 +1,5 @@
-<div class="container mx-auto">
-    <div class="grid grid-cols-none gap-6 md:grid-cols-2">
+<div class="">
+    <div class="sm:grid grid-cols-1 md:grid grid-cols-2">
         <div>
             <div class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 mb-2 m-20 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">
                 Hai, Saya Mochi
@@ -7,8 +7,8 @@
             <div id="fadeNama" class="mt-5 m-20 text-2xl font-bold tracking-tight leading-relaxed text-gray-600 md:text-3xl lg:text-4xl">
                 Saya adalah junior web developer. Jangan samakan saya dengan para pemula itu. Karena saya masih kalah jauh sama mereka. *muka sedih*</div>
         </div>
-        <div class="">
-            <img id="fadeGambar" src="/assets/img/LogoMochi.png" alt="">
-        </div>
+
+        <div><img class="-z-50" id="fadeGambar" src="/assets/img/LogoMochi.png" alt=""></div>
+
     </div>
 </div>
