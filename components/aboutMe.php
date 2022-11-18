e42651ed506fd35ffc4d59085bff6c485a91943c
eazycchi <mochizza123@gmail.com>
2022-11-18 16:40:10 +0700

    revisi tampilan

--- components/aboutMe.php
+++ components/aboutMe.php
@@ -1,12 +1,12 @@
-<div class="grid grid-cols-2 sm:grid-cols-1">
+<div class="grid md:grid-cols-2 sm:grid-cols-1">
     <div class="">
-        <div class="mb-2 m-20 text-4xl font-extrabold tracking-tight leading-none dark:text-white text-gray-900 md:text-3xl lg:text-3xl">
+        <div class="mb-2 text-4xl font-extrabold tracking-tight leading-none dark:text-white text-gray-900 md:text-3xl lg:text-3xl">
             <span class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"> Hai, Saya Mochi</span>
         </div>
-        <div id="fadeNama" class="dark:text-white mt-5 m-20 text-2xl font-bold tracking-tight leading-relaxed text-gray-600 md:text-xl lg:text-2xl">
+        <div id="fadeNama" class="dark:text-white  text-2xl font-bold tracking-tight leading-relaxed text-gray-600 md:text-xl lg:text-2xl">
             Saya adalah junior web developer. Jangan samakan saya dengan para pemula itu. Karena saya masih kalah jauh sama mereka. *muka sedih*</div>
-        <hr id="fadeNama" class="mt-5 m-20">
-        <div id="fadeNama" class="dark:text-white mt-5 m-20 text-2xl font-bold tracking-tight leading-relaxed text-gray-600 md:text-xl lg:text-2xl">
+        <hr id="fadeNama" class="">
+        <div id="fadeNama" class="dark:text-white  text-2xl font-bold tracking-tight leading-relaxed text-gray-600 md:text-xl lg:text-2xl">
             Kalo web ini udah jadi, gw mau copas, gw mau taruh ke linkedin gw, gamau tau, atau pemilik akun ini harus bayar 500 juta. <br>-Gw hacker
         </div>
     </div>
