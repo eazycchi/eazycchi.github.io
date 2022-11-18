31c6ff107ebe7bb40fcaf24540b1eceba5527e9c
eazycchi <mochizza123@gmail.com>
2022-11-18 11:40:56 +0700

    delete dark mode

--- components/aboutMe.php
+++ components/aboutMe.php
@@ -1,10 +1,10 @@
 <div class="container mx-auto">
     <div class="grid grid-cols-none gap-6 md:grid-cols-2">
         <div>
-            <div class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 mb-2 m-20 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+            <div class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 mb-2 m-20 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">
                 Hai, Saya Mochi
             </div>
-            <div id="fadeNama" class="mt-5 m-20 text-2xl font-bold tracking-tight leading-relaxed text-gray-600 md:text-3xl lg:text-4xl dark:text-white">
+            <div id="fadeNama" class="mt-5 m-20 text-2xl font-bold tracking-tight leading-relaxed text-gray-600 md:text-3xl lg:text-4xl">
                 Saya adalah junior web developer. Jangan samakan saya dengan para pemula itu. Karena saya masih kalah jauh sama mereka. *muka sedih*</div>
         </div>
         <div class="">
