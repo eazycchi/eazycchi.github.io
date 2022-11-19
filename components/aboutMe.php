2ad2f02851846bb9d99d5bd0c5e68861ccd576be
eazycchi <mochizza123@gmail.com>
2022-11-19 12:02:24 +0700

    revisi tampilan

--- components/aboutMe.php
+++ components/aboutMe.php
@@ -1,15 +1,17 @@
 <div class="grid md:grid-cols-2 sm:grid-cols-1">
     <div class="">
-        <div class="mb-2 text-4xl font-extrabold tracking-tight leading-none dark:text-white text-gray-900 md:text-3xl lg:text-3xl">
+        <div class="m-2 text-center text-4xl font-extrabold tracking-tight leading-none dark:text-white text-gray-900 md:text-3xl lg:text-3xl">
             <span class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"> Hai, Saya Mochi</span>
         </div>
-        <div id="fadeNama" class="dark:text-white  text-2xl font-bold tracking-tight leading-relaxed text-gray-600 md:text-xl lg:text-2xl">
+        <div id="fadeNama" class="dark:text-white m-2 text-2xl font-bold tracking-tight leading-relaxed text-gray-600 md:text-xl lg:text-2xl">
             Saya adalah junior web developer. Jangan samakan saya dengan para pemula itu. Karena saya masih kalah jauh sama mereka. *muka sedih*</div>
         <hr id="fadeNama" class="">
-        <div id="fadeNama" class="dark:text-white  text-2xl font-bold tracking-tight leading-relaxed text-gray-600 md:text-xl lg:text-2xl">
+        <div id="fadeNama" class="dark:text-white m-2 text-2xl font-bold tracking-tight leading-relaxed text-gray-600 md:text-xl lg:text-2xl">
             Kalo web ini udah jadi, gw mau copas, gw mau taruh ke linkedin gw, gamau tau, atau pemilik akun ini harus bayar 500 juta. <br>-Gw hacker
         </div>
     </div>
+    <div>
+        <img class="-z-50" id="fadeAbout" src="/assets/img/LogoMochi.svg" alt="">
+        <img class="-z-50" id="fadeAbout2" src="/assets/img/LogoMochi.svg" alt="">
+    </div>
 </div>
-<img class="-z-50" id="fadeAbout" src="/assets/img/LogoMochi.svg" alt="">
-<img class="-z-50" id="fadeAbout2" src="/assets/img/LogoMochi.svg" alt="">
