a40adeaabcfe04de0ce1d1e1c244bc6c7f0fc16c
eazycchi <mochizza123@gmail.com>
2022-11-21 07:59:59 +0700

    absen pagi

--- components/project.php
+++ components/project.php
@@ -1,34 +1,4 @@
 <div class="w-screen flex justify-center items-center ">
     <div class="py-2 text-4xl font-extrabold tracking-tight leading-none dark:text-white text-gray-900 md:text-5xl lg:text-6xl">
-        Daftar Project</div>
-</div>
-<div>
-
-</div>
-<div class="grid grid-cols-1 gap-6 sm:grid-cols-3">
-    <div class="max-w-sm dark:bg-slate-800 bg-white rounded-lg border dark:border-slate-800 border-gray-200 shadow-md">
-        <a href="#">
-            <img class="rounded-t-lg" src="https://img.era.id/N_gmQ0pRGFpWHeUgv5tCEfpvUBGhW5OOi_QM5snA0PM/rs:fill:1280:720/g:sm/bG9jYWw6Ly8vcHVibGlzaGVycy8zNzY0My8yMDIwMDkxMTA5MzUtbWFpbi5jcm9wcGVkXzE1OTk3OTE3OTYuY3JvcHBlZF8xNTk5NzkxODQxLnBuZw.jpg" alt="" />
-        </a>
-        <div class="p-5">
-            <a href="">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight dark:text-white text-gray-900">Noteworthy technology
-                    acquisitions 2021</h5>
-            </a>
-            <p class="mb-3 font-normal dark:text-white text-gray-700">Here are the biggest enterprise technology
-                acquisitions of 2021 so far, in reverse chronological order.</p>
-            <a href="" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-                Read more
-                <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                </svg>
-            </a>
-        </div>
-    </div>
-    <p class="mb-3 font-light dark:text-white text-gray-500">Deliver great service experiences fast - without the
-        complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy
-        changes with ease, with a complete audit trail for every change.</p>
-    <p class="mb-3 font-light dark:text-white text-gray-500">Deliver great service experiences fast - without the
-        complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy
-        changes with ease, with a complete audit trail for every change.</p>
+        Daftar Project </div>
 </div>
